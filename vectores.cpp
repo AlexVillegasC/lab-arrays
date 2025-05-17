57bf2fa641736292aaaee6f8c862b02d9e98424f
Nabi28 <grijalbadfereshtte03@gmail.com>
2025-05-17 15:03:17 -0600

    test multiplicacion

--- vectores.cpp
+++ vectores.cpp
@@ -1,8 +1,19 @@
-#include<iostream>
+#include <iostream>
 
 using namespace std;
-
-int main (){
-    
+int muliplicacion(int a, int b)
+{
+    int a, b;
+    cout << "Ingrese el primer numerio:";
+    cin >> a;
+    cout << "Ingrese el segundo numerio:";
+    cin >> b;
+    cout << "El resultado de la multiplicacion es: " << a << " * " << b << " = " << muliplicacion(a, b) << endl;
+    return 0;
+    return a * b;
+}
+int main()
+{
+    muliplicacion(a, b);
     return 0;
 }
