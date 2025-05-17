9383dfa5d4068c3e5256647fd8180ebe7a36c11d
alexvillegasopenpath <alex@openpath.io>
2025-05-17 14:28:38 -0600

    suma

--- vectores.cpp
+++ vectores.cpp
@@ -2,7 +2,27 @@
 
 using namespace std;
 
-int main (){
+int suma(int a, int b)
+{
+   
+    std::cout << "Ingrese el primer numero: ";       
+    std::cin >> a;
     
+    std::cout << "Ingrese el segundo numero: "; 
+    cin >> b;
+
+    cout << "La suma de " << a << " + " << b << " es: " << (a + b) << endl;
+
+    return a + b;
+}
+
+int main ()
+{
+
+    int a, b;
+
+ 
+    suma(a, b);
+  
     return 0;
 }
