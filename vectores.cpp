c1b9050b39d4c58ae6dfdb055d51fb41acb86871
alexvillegasopenpath <alex@openpath.io>
2025-05-19 19:38:44 -0600

    La multiplicacion y la resta fueron fusionados correctamente

--- vectores.cpp
+++ vectores.cpp
@@ -1,19 +1,36 @@
 #include <iostream>
 
 using namespace std;
-int muliplicacion(int a, int b)
+int multiplicacion()
 {
     int a, b;
     cout << "Ingrese el primer numerio:";
     cin >> a;
     cout << "Ingrese el segundo numerio:";
     cin >> b;
-    cout << "El resultado de la multiplicacion es: " << a << " * " << b << " = " << muliplicacion(a, b) << endl;
+    cout << "El resultado de la multiplicacion es: " << a << " * " << b << " = " << multiplicacion(a, b) << endl;
     return 0;
     return a * b;
 }
+
+void resta(){
+    int a;
+    int b;
+    cout << "\033[2J\033[H";
+    cout<<"ingrese numero 1: "<<endl;
+    cin>>a;
+
+    cout<<"ingrese numero 2: "<<endl;
+    cin>>b;
+
+    cout<<"la resta es: "<< a - b <<endl; 
+    system("pause");
+    cout << "\033[2J\033[H";
+}
+
 int main()
 {
-    muliplicacion(a, b);
+    multiplicacion();
+    resta();
     return 0;
 }
