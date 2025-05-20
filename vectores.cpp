b2de371962360c365133dbb9f796012cabf279fe
alexvillegasopenpath <alex@openpath.io>
2025-05-19 19:49:50 -0600

    integracion con codigo de suma

--- vectores.cpp
+++ vectores.cpp
@@ -1,16 +1,15 @@
 #include <iostream>
 
 using namespace std;
-int multiplicacion()
+
+void multiplicacion()
 {
     int a, b;
     cout << "Ingrese el primer numerio:";
     cin >> a;
     cout << "Ingrese el segundo numerio:";
     cin >> b;
-    cout << "El resultado de la multiplicacion es: " << a << " * " << b << " = " << multiplicacion(a, b) << endl;
-    return 0;
-    return a * b;
+    cout << "El resultado de la multiplicacion es: " << a << " * " << b << " = " << a * b << endl;        
 }
 
 void resta(){
@@ -28,9 +27,27 @@ void resta(){
     cout << "\033[2J\033[H";
 }
 
+void suma()
+{
+    int a;
+    int b;
+    cout << "\033[2J\033[H";
+    cout<<"ingrese numero 1: "<<endl;
+    cin>>a;
+
+    cout<<"ingrese numero 2: "<<endl;
+    cin>>b;
+
+    cout<<"la resta es: "<< a + b <<endl; 
+    system("pause");
+    cout << "\033[2J\033[H";
+}
+
 int main()
 {
     multiplicacion();
     resta();
+    suma();
+    cout << "Fin del programa" << endl;
     return 0;
 }
